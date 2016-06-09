3a50760f3b0143f67578530457a6f498b0cadfb3
finley007 <finley916@gmail.com>
2016-06-09 23:02:38 +0800

    Add open table prompt

--- XiaoWeiCanTing/js/table.js
+++ XiaoWeiCanTing/js/table.js
@@ -1,10 +1,13 @@
 (function(owner) {
 	
+	//桌台区域列表
 	owner.areaInfo = [{"code": "", "label": "全部区域"}, {"code": "1", "label": "大堂"}, {"code": "2", "label": "春"}, {"code": "3", "label": "夏"}, {"code": "4", "label": "秋"}, {"code": "5", "label": "冬"}];
+	//桌台状态列表
 	owner.statusInfo = [{"code": "", "label": "全部状态"}, {"code": "idle", "label": "空闲"}, {"code": "ordered", "label": "已开台"}, {"code": "started", "label": "已下单"}, {"code": "payed", "label": "已支付"}];
 	
 	owner.status = "";
 	
+	//桌台菜单控制：不同状态不同的可选菜单
 	var menuConfig = {
 	    "idle": [
 	        {
@@ -55,6 +58,7 @@
 		return "";
 	}
 	
+	//生成桌台HTML
 	function createTableHTML(info) {
 		var result = "";
 		info = info || {};
@@ -75,6 +79,7 @@
 		return result;
 	}
 	
+	//构造添加桌台模块并添加针对每个桌台的单击事件监听
 	owner.addTables = function(parent, info, callback) {
 		if (typeof(parent) == "undefined" || parent == null) {
 			return;
@@ -117,6 +122,7 @@
 		return {"content" : content, "count" : count};
 	}
 	
+	//过滤桌台，目前只支持根据状态和区域过滤
 	owner.filterTable = function(tableInfo, status, area) {
 		if (status.trim() == "" && area.trim() == "") {
 			return tableInfo;
@@ -137,6 +143,7 @@
 		return JSON.stringify({"tables" : newTableList});
 	}
 	
+	//根据桌台编号搜索桌台
 	owner.filterTableByNo = function(tableInfo, no) {
 		if ("" == no.trim()) {
 			return tableInfo;
@@ -154,5 +161,29 @@
 			}
 		}
 		return JSON.stringify({"tables" : newTableList});
+	}
+	
+	//开台提示
+	owner.openTablePrompt = function(openOrderFunc, openFunc) {
+		mui.prompt(' ', '', '开台提示', ['开台点菜', '开台', '取消'], function(e) {
+					if (e.index == 0) {
+						openOrderFunc();
+					} else if (e.index == 1) {
+						openFunc();
+					}}, 'div');
+		document.querySelector('.mui-popup-input').innerHTML = '<div style="margin-top: 10px;">请输入就餐人数<div class="mui-numbox" data-numbox-min="1" data-numbox-max="20" style="margin-left: 10px;"><button class="mui-btn mui-btn-numbox-minus" type="button">-</button><input id="test" class="mui-input-numbox" type="number" /><button class="mui-btn mui-btn-numbox-plus" type="button">+</button></div></div>';
+		mui.ready(function() {
+			mui('.mui-numbox').numbox();
+		})
+	}
+	
+	//TODO 调用开台服务
+	owner.callOpenTableService = function() {
+		alert("调用开台服务");
+	}
+	
+	//TODO 调用清台服务
+	owner.callClearTableService = function() {
+		alert("调用清台服务");
 	}
 }(window.table = {}));
